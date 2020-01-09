e7142282e88b3beeced655c8c5e1091e9b812ae4
Matt McElwee <mmcelwee@dmsi.com>
2020-01-08 18:21:33 -0600

    Added sideboard relationships to the chief

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,7 @@ import d from './utils/d';
 import make from './utils/make';
 import { createDeity, Deity } from './deity';
 import { generateDisposition, Disposition } from './dispositions';
+import { createSideboardRelationship, invertRelationship } from './relationships';
 
 // TODO: workshop these. I like them conceptually, especially for competing
 // pantheons, but need to think through implications/balance
@@ -14,6 +15,7 @@ interface Pantheon {
   dispositions: Array<Disposition>;
   age: typeof ages[number];
   chief?: string; // deity.id
+  chiefSpouse?: string;
   deities: Array<Deity>;
   seed: string;
 }
@@ -22,9 +24,27 @@ const createPantheon = (seed: string = sower.silly()): Pantheon => {
   const dispositions = make(3).map((_, idx) => generateDisposition(seed + (idx * 4.28).toString()));
 
   const chief = createDeity(seed + 'chief', dispositions, []);
-  const sideBoard = make(10, seed).reduce((acc, _, idx) => {
-    const currentArchs = [chief.archetype, ...acc.map(({ archetype }) => archetype)];
-    acc.push(createDeity(seed + (idx * 3.14).toString() + 'sideboard', dispositions, currentArchs));
+
+  const sideBoard = make(10, seed)
+    .reduce((acc, _, idx) => {
+      const currentArchs = [chief.archetype, ...acc.map(({ archetype }) => archetype)];
+      acc.push(
+        createDeity(seed + (idx * 3.14).toString() + 'sideboard', dispositions, currentArchs),
+      );
+      return acc;
+    }, [])
+    .map(createSideboardRelationship(chief.id, seed));
+
+  chief.relationships = sideBoard.reduce((acc, { id, relationships }) => {
+    if (relationships.length) {
+      acc.push(
+        ...relationships.map((r) => {
+          const rel = invertRelationship(r, chief);
+          rel.deityId = id;
+          return rel;
+        }),
+      );
+    }
     return acc;
   }, []);
 
@@ -42,14 +62,14 @@ const createPantheon = (seed: string = sower.silly()): Pantheon => {
       : make(8, seed + 'spouse-consorts').map(() => ({ first: spouse.id, second: 'anon' }))),
   ];
 
+  console.log('SIDEBOARD', sideBoard);
   console.log('CHIEF', chief);
-  console.log('SPOUSE', spouse);
-  console.log('CONSORTS', consorts);
   console.log(seed);
 
   return {
     deities: [],
     chief: chief.id,
+    chiefSpouse: !selfReproducing && spouse.id,
     age: pick(ages, seed),
     dispositions,
     seed,
--- src/relationships.ts
+++ src/relationships.ts
@@ -1,44 +1,92 @@
-import { Deity } from './deity';
+import cloneDeep from 'clone-deep';
+
+import { Deity, Relationship } from './deity';
+import d from './utils/d';
 
 const RELATIONSHIP_TYPES = [
   'father',
   'mother',
+  'son',
+  'daughter',
   'sister',
   'brother',
   'nephew',
   'niece',
   'uncle',
   'aunt',
-  'spouse',
-  'consort',
-  'friend',
-  'rival',
-  'nemesis',
 ] as const;
 
 export type RelationshipTypes = typeof RELATIONSHIP_TYPES[number];
 
-const relTree = {
-  family: {
-    female: ['sister', 'mother', 'niece', 'aunt', 'spouse'],
-    male: ['father', 'brother', 'nephew', 'uncle', 'spouse'],
-  },
-  positive: ['consort', 'friend'],
-  negative: ['rival', 'nemesis'],
+const decimal = (seed) => (d(100, seed) - 1) / 100;
+
+export const createSideboardRelationship = (chiefId: string, seed: string) => (
+  deity: Deity,
+): Deity => {
+  const isRelated = d(3, seed + 'related') === 1;
+
+  if (!isRelated) return deity;
+
+  const isSibling = d(2, seed + 'sibling') === 1;
+
+  let type;
+  if (isSibling) {
+    type = deity.gender !== 'female' ? 'brother' : 'sister';
+  } else {
+    type = deity.gender !== 'female' ? 'uncle' : 'aunt';
+  }
+
+  return {
+    ...deity,
+    relationships: [
+      {
+        deityId: chiefId,
+        type,
+        consort: false,
+        eris: decimal(seed + 'eris'),
+        eros: decimal(seed + 'eros'),
+        philo: decimal(seed + 'philo'),
+        agape: decimal(seed + 'agape'),
+      },
+    ],
+  };
 };
 
-export const createRelationships = (deities: Array<Deity>, seed: string) => {
-  const relationships = {};
+export const invertRelationship = (relationship: Relationship, deity: Deity): Relationship => {
+  const newRelationship = cloneDeep(relationship);
 
-  // Identify all the men
-  const potentialFathers = deities.filter(({ gender }) => gender === 'male');
-  console.log(potentialFathers);
-
-  // start off with the family so we don't accidentally get overlaps
-  // commit family nodes
-
-  // randomly pick out consorts, friends, rivals, and nemeses
-  // limit based on the size of the pantheon
+  switch (relationship.type) {
+    case 'aunt':
+    case 'uncle':
+      return {
+        ...newRelationship,
+        type: deity.gender !== 'female' ? 'nephew' : 'niece',
+      };
+    case 'sister':
+    case 'brother':
+      return {
+        ...newRelationship,
+        type: deity.gender !== 'female' ? 'brother' : 'sister',
+      };
+    case 'nephew':
+    case 'niece':
+      return {
+        ...newRelationship,
+        type: deity.gender !== 'female' ? 'uncle' : 'aunt',
+      };
+    case 'mother':
+    case 'father':
+      return {
+        ...newRelationship,
+        type: deity.gender !== 'female' ? 'son' : 'daughter',
+      };
+    case 'daughter':
+    case 'son':
+      return {
+        ...newRelationship,
+        type: deity.gender !== 'female' ? 'father' : 'mother',
+      };
+  }
 };
 
 export default RELATIONSHIP_TYPES;
