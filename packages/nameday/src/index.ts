16dd3c5641345f5d35f1ef5bf6ed503a037e7395
Matt McElwee <mmcelwee@dmsi.com>
2020-01-26 20:19:45 -0600

    Added barrel file for nameday

---
+++ packages/nameday/src/index.ts
@@ -0,0 +1,3 @@
+export { default as parseDataset } from './parseDataset';
+export { default as parseName } from './parseName';
+export { default as datasetToSiblingTree } from './datasetToSiblingTree';
