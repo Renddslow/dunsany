57e33269c83af07cf4ef64a6ba44d868cb9ae0ef
Matt McElwee <mmcelwee@dmsi.com>
2020-01-27 22:03:15 -0600

    Updated minmax function to use Math.min and Math.max

--- packages/nameday/src/utils/getMinMax.ts
+++ packages/nameday/src/utils/getMinMax.ts
@@ -9,8 +9,8 @@ const getMinMax = (names: Array<string>): MinMax =>
       const len = name.length;
       if (acc.min === 0) acc.min = len;
       if (acc.max === 0) acc.max = len;
-      if (name.length > acc.max) acc.max = len;
-      if (name.length < acc.min) acc.min = len;
+      acc.min = Math.min(acc.min, len);
+      acc.max = Math.max(acc.max, len);
 
       return acc;
     },
